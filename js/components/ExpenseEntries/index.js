762271362b1bb054f25f598ca46b998df540a58f
Erik Steinberger <54215204+shelterandtrees@users.noreply.github.com>
2020-03-28 13:40:05 -0700

    Add files via upload

---
+++ js/components/ExpenseEntries/index.js
@@ -0,0 +1,15 @@
+import { connect } from 'react-redux';
+import ExpenseEntries from './ExpenseEntries';
+
+// this function takes the store and returns an object
+// that's passed to the props of the component.
+
+function mapStoreToProps(store) {
+    return {
+        description: store.expense.description,
+        amount: store.expense.amount,
+        lineItems: store.expense.lineItems
+    };
+}
+
+export default connect(mapStoreToProps)(ExpenseEntries);
---
+++ js/components/IncomeEntries/incomeReducer.js
@@ -0,0 +1,9 @@
+const defaultState = {
+    description: '',
+    amount: '',
+    lineItems: []
+};
+
+export default function incomeReducer (state = defaultState, action) {
+    return state;
+}
